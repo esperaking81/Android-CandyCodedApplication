98ae6554827930d923f82ad08184089f36c007e8
Espéra AWO <esperaking81@gmail.com>
2021-12-13 14:23:45 +0100

    create share intent

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -73,6 +73,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
 
